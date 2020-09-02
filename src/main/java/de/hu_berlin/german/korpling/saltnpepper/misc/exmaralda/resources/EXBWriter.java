f2457a2ff9bb04c5b702a3d85bd5306b12d7a3d3
Thomas Krause <krauseto@hu-berlin.de>
2020-09-02 10:15:36 +0200

    Fix incorrect generation of language elements (value is in attribute, not in as text sub node)

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/misc/exmaralda/resources/EXBWriter.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/misc/exmaralda/resources/EXBWriter.java
@@ -29,242 +29,224 @@ import de.hu_berlin.german.korpling.saltnpepper.misc.exmaralda.TLI;
 import de.hu_berlin.german.korpling.saltnpepper.misc.exmaralda.Tier;
 import de.hu_berlin.german.korpling.saltnpepper.misc.exmaralda.UDInformation;
 
-
-public class EXBWriter 
-{
+public class EXBWriter {
 	/**
 	 * Encoding in which file has to be written
 	 */
-	private String encoding= "UTF-8";
-	
+	private String encoding = "UTF-8";
+
 	/**
 	 * Stream in which the file content is written.
 	 */
-	PrintStream fileWriter= null;
-	
+	PrintStream fileWriter = null;
+
 	/**
 	 * Stores a a treetagger-model into tab-seperated file.
 	 */
-	public void write(BasicTranscription basicTranscription, URI uri) throws java.io.IOException
-	{
-		//TODO: change fileWriter to StringBuffer, at the end of process write data into file
-		this.fileWriter= new PrintStream(new FileOutputStream(uri.toFileString()), true, this.encoding);
-		try 
-		{
-			//print head
+	public void write(BasicTranscription basicTranscription, URI uri) throws java.io.IOException {
+		// TODO: change fileWriter to StringBuffer, at the end of process write data
+		// into file
+		this.fileWriter = new PrintStream(new FileOutputStream(uri.toFileString()), true, this.encoding);
+		try {
+			// print head
 			fileWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-			{//print basic-transcription
+			{// print basic-transcription
 				fileWriter.println("<basic-transcription>");
-				{//print head
+				{// print head
 					fileWriter.println("<head>");
-					{//print meta-information
+					{// print meta-information
 						fileWriter.println("<meta-information>");
-						{//project-name
+						{// project-name
 							fileWriter.print("<project-name>");
-							if (	(basicTranscription.getMetaInformation()!= null) &&
-									(basicTranscription.getMetaInformation().getProjectName()!= null))
+							if ((basicTranscription.getMetaInformation() != null)
+									&& (basicTranscription.getMetaInformation().getProjectName() != null))
 								fileWriter.print(basicTranscription.getMetaInformation().getProjectName());
 							fileWriter.print("</project-name>");
 						}
-						{//transcription-name
+						{// transcription-name
 							fileWriter.print("<transcription-name>");
-							if (	(basicTranscription.getMetaInformation()!= null) &&
-									(basicTranscription.getMetaInformation().getTranscriptionName()!= null))
+							if ((basicTranscription.getMetaInformation() != null)
+									&& (basicTranscription.getMetaInformation().getTranscriptionName() != null))
 								fileWriter.print(basicTranscription.getMetaInformation().getTranscriptionName());
 							fileWriter.print("</transcription-name>");
 						}
-						{//referenced-file
+						{// referenced-file
 							fileWriter.print("<referenced-file");
 							fileWriter.print(" url=\"");
-							if (	((basicTranscription.getMetaInformation()!= null)) &&
-									(basicTranscription.getMetaInformation().getReferencedFile()!= null))
+							if (((basicTranscription.getMetaInformation() != null))
+									&& (basicTranscription.getMetaInformation().getReferencedFile() != null))
 								fileWriter.print(basicTranscription.getMetaInformation().getReferencedFile());
 							fileWriter.print("\">");
 							fileWriter.print("</referenced-file>");
 						}
-						{//ud-information
-							
-							//if ud-information exists
-							if (	(basicTranscription.getMetaInformation()!= null)&&
-									(basicTranscription.getMetaInformation().getUdMetaInformations()!= null)&&
-									(basicTranscription.getMetaInformation().getUdMetaInformations().size()> 0))
-							{	
+						{// ud-information
+
+							// if ud-information exists
+							if ((basicTranscription.getMetaInformation() != null)
+									&& (basicTranscription.getMetaInformation().getUdMetaInformations() != null)
+									&& (basicTranscription.getMetaInformation().getUdMetaInformations().size() > 0)) {
 								fileWriter.print("<ud-meta-information>");
-								for (UDInformation udInfo: basicTranscription.getMetaInformation().getUdMetaInformations())
-								{
-									//udinfo cannot has an empty name
-									if (	(udInfo.getAttributeName()!= null) &&
-											(!udInfo.getAttributeName().equals("")))
-									{	
+								for (UDInformation udInfo : basicTranscription.getMetaInformation()
+										.getUdMetaInformations()) {
+									// udinfo cannot has an empty name
+									if ((udInfo.getAttributeName() != null)
+											&& (!udInfo.getAttributeName().equals(""))) {
 										fileWriter.print("<ud-information");
-										fileWriter.print(" attribute-name=\""+udInfo.getAttributeName()+"\">");
-										if (udInfo.getValue() != null) 
+										fileWriter.print(" attribute-name=\"" + udInfo.getAttributeName() + "\">");
+										if (udInfo.getValue() != null)
 											fileWriter.print(udInfo.getValue());
 										fileWriter.print("</ud-information>");
 									}
 								}
 								fileWriter.print("</ud-meta-information>");
 							}
-							//print empty ud-information
-							else 
-							{
+							// print empty ud-information
+							else {
 								fileWriter.print("<ud-meta-information/>");
 							}
 						}
-						{//comment
+						{// comment
 							fileWriter.print("<comment>");
-							if (	((basicTranscription.getMetaInformation()!= null)) &&
-									(basicTranscription.getMetaInformation().getComment()!= null)) {
-                  fileWriter.print(basicTranscription.getMetaInformation().getComment());
-              }
+							if (((basicTranscription.getMetaInformation() != null))
+									&& (basicTranscription.getMetaInformation().getComment() != null)) {
+								fileWriter.print(basicTranscription.getMetaInformation().getComment());
+							}
 							fileWriter.print("</comment>");
 						}
-						{//transcription-convention
+						{// transcription-convention
 							fileWriter.print("<transcription-convention>");
-							if (	(basicTranscription.getMetaInformation()!= null)&&
-									(basicTranscription.getMetaInformation().getTranscriptionConvention()!= null))
+							if ((basicTranscription.getMetaInformation() != null)
+									&& (basicTranscription.getMetaInformation().getTranscriptionConvention() != null))
 								fileWriter.print(basicTranscription.getMetaInformation().getTranscriptionConvention());
 							fileWriter.print("</transcription-convention>");
 						}
 						this.fileWriter.println("</meta-information>");
 					}
-					{//speakertable
-						if (basicTranscription.getSpeakertable()!= null)
-						{
+					{// speakertable
+						if (basicTranscription.getSpeakertable() != null) {
 							fileWriter.println("<speakertable>");
-							for (Speaker speaker : basicTranscription.getSpeakertable())
-							{//speaker
+							for (Speaker speaker : basicTranscription.getSpeakertable()) {// speaker
 								fileWriter.print("<speaker");
-								if (speaker.getId()!= null)
-									fileWriter.print(" id=\""+speaker.getId()+ "\">");
-								else fileWriter.print(">");
-								{//abbreviation
+								if (speaker.getId() != null)
+									fileWriter.print(" id=\"" + speaker.getId() + "\">");
+								else
+									fileWriter.print(">");
+								{// abbreviation
 									fileWriter.println();
 									fileWriter.print("<abbreviation>");
-									if (speaker.getAbbreviation()!= null)	
+									if (speaker.getAbbreviation() != null)
 										fileWriter.print(speaker.getAbbreviation());
 									fileWriter.println("</abbreviation>");
 								}
-								{//sex
+								{// sex
 									fileWriter.print("<sex");
 									if (speaker.isSetSex())
-										fileWriter.print(" value=\""+speaker.getSex()+ "\">");
-									else fileWriter.print(">");
+										fileWriter.print(" value=\"" + speaker.getSex() + "\">");
+									else
+										fileWriter.print(">");
 									fileWriter.println("</sex>");
 								}
-								{//languages-used
+								{// languages-used
 									fileWriter.print("<languages-used>");
-									for(String lang: speaker.getLanguageUsed())
-									{
-										fileWriter.print("<language>");
+									for (String lang : speaker.getLanguageUsed()) {
+										fileWriter.print("<language lang=\"");
 										fileWriter.print(lang);
-										fileWriter.println("</language>");
-									}	
+										fileWriter.println("\"></language>");
+									}
 									fileWriter.println("</languages-used>");
 								}
-								{//l1
+								{// l1
 									fileWriter.print("<l1>");
-									for(String lang: speaker.getL1())
-									{
-										fileWriter.print("<language>");
+									for (String lang : speaker.getL1()) {
+										fileWriter.print("<language lang=\"");
 										fileWriter.print(lang);
 										fileWriter.println("</language>");
-									}	
+									}
 									fileWriter.println("</l1>");
 								}
-								{//l2
+								{// l2
 									fileWriter.print("<l2>");
-									for(String lang: speaker.getL2())
-									{
-										fileWriter.print("<language>");
+									for (String lang : speaker.getL2()) {
+										fileWriter.print("<language lang=\"");
 										fileWriter.print(lang);
 										fileWriter.println("</language>");
-									}	
+									}
 									fileWriter.println("</l2>");
 								}
-								{//ud-information
+								{// ud-information
 									fileWriter.print("<ud-speaker-information>");
-									for (UDInformation udInfo: speaker.getUdSpeakerInformations())
-									{
-										//udinfo cannot has an empty name
-										if (	(udInfo.getAttributeName()!= null) &&
-												(!udInfo.getAttributeName().equals("")))
-										{	
+									for (UDInformation udInfo : speaker.getUdSpeakerInformations()) {
+										// udinfo cannot has an empty name
+										if ((udInfo.getAttributeName() != null)
+												&& (!udInfo.getAttributeName().equals(""))) {
 											fileWriter.print("<ud-information");
-											fileWriter.print(" attribute-name=\""+udInfo.getAttributeName()+"\">");
-											if (udInfo.getValue() != null) 
+											fileWriter.print(" attribute-name=\"" + udInfo.getAttributeName() + "\">");
+											if (udInfo.getValue() != null)
 												fileWriter.print(udInfo.getValue());
 											fileWriter.print("</ud-information>");
 										}
 									}
 									fileWriter.println("</ud-speaker-information>");
-									
+
 								}
-								{//comment
+								{// comment
 									fileWriter.print("<comment>");
-									if (speaker.getComment()!= null)
+									if (speaker.getComment() != null)
 										fileWriter.print(speaker.getComment());
 									fileWriter.println("</comment>");
 								}
 								fileWriter.println("</speaker>");
-							}	
+							}
 							fileWriter.println("</speakertable>");
-						}	
+						}
 					}
 					fileWriter.println("</head>");
 				}
-				{//basic-body
+				{// basic-body
 					fileWriter.println("<basic-body>");
-					if (basicTranscription.getCommonTimeLine()!= null)
-					{//common timeline
+					if (basicTranscription.getCommonTimeLine() != null) {// common timeline
 						fileWriter.println("<common-timeline>");
-						for (TLI tli: basicTranscription.getCommonTimeLine().getTLIs())
-						{//tli
-							StringBuilder element= new StringBuilder();
+						for (TLI tli : basicTranscription.getCommonTimeLine().getTLIs()) {// tli
+							StringBuilder element = new StringBuilder();
 							element.append("<tli");
-							if (tli.getId()!= null)
-								element.append(" id=\""+tli.getId()+"\"");
-							if (tli.getTime()!= null)
-								element.append(" time=\""+tli.getTime()+"\"");
+							if (tli.getId() != null)
+								element.append(" id=\"" + tli.getId() + "\"");
+							if (tli.getTime() != null)
+								element.append(" time=\"" + tli.getTime() + "\"");
 							if (tli.isSetType())
-								element.append(" type=\""+tli.getType()+"\"");
+								element.append(" type=\"" + tli.getType() + "\"");
 							element.append("/>");
 							fileWriter.println(element);
-//							fileWriter.println("</tli>");
-						}	
+							// fileWriter.println("</tli>");
+						}
 						fileWriter.println("</common-timeline>");
-					}	
-					for (Tier tier: basicTranscription.getTiers())
-					{//tier
-						
-						StringBuilder element= new StringBuilder();
+					}
+					for (Tier tier : basicTranscription.getTiers()) {// tier
+
+						StringBuilder element = new StringBuilder();
 						element.append("<tier");
-						if (tier.getId()!= null)
-							element.append(" id=\""+tier.getId()+"\"");
-						if (tier.getSpeaker()!= null)
-							element.append(" speaker=\""+tier.getSpeaker().getId()+"\"");
-						if (tier.getCategory()!= null)
-							element.append(" category=\""+tier.getCategory()+"\"");
+						if (tier.getId() != null)
+							element.append(" id=\"" + tier.getId() + "\"");
+						if (tier.getSpeaker() != null)
+							element.append(" speaker=\"" + tier.getSpeaker().getId() + "\"");
+						if (tier.getCategory() != null)
+							element.append(" category=\"" + tier.getCategory() + "\"");
 						if (tier.isSetType())
-							element.append(" type=\""+tier.getType()+"\"");
-						if (tier.getDisplayName()!= null)
-							element.append(" display-name=\""+tier.getDisplayName()+"\"");
+							element.append(" type=\"" + tier.getType() + "\"");
+						if (tier.getDisplayName() != null)
+							element.append(" display-name=\"" + tier.getDisplayName() + "\"");
 						element.append(">");
 						fileWriter.print(element);
-						{//ud-information
-							if (	(tier.getUdTierInformations()!= null) &&
-									(tier.getUdTierInformations().size()> 0))
-							{	
+						{// ud-information
+							if ((tier.getUdTierInformations() != null) && (tier.getUdTierInformations().size() > 0)) {
 								fileWriter.print("<ud-tier-information>");
-								for (UDInformation udInfo: tier.getUdTierInformations())
-								{
-									//udinfo cannot has an empty name
-									if (	(udInfo.getAttributeName()!= null) &&
-											(!udInfo.getAttributeName().equals("")))
-									{	
+								for (UDInformation udInfo : tier.getUdTierInformations()) {
+									// udinfo cannot has an empty name
+									if ((udInfo.getAttributeName() != null)
+											&& (!udInfo.getAttributeName().equals(""))) {
 										fileWriter.print("<ud-information");
-										fileWriter.print(" attribute-name=\""+udInfo.getAttributeName()+"\">");
-										if (udInfo.getValue() != null) 
+										fileWriter.print(" attribute-name=\"" + udInfo.getAttributeName() + "\">");
+										if (udInfo.getValue() != null)
 											fileWriter.print(udInfo.getValue());
 										fileWriter.print("</ud-information>");
 									}
@@ -272,50 +254,44 @@ public class EXBWriter
 								fileWriter.print("</ud-tier-information>");
 							}
 						}
-						if (tier.getEvents().size()> 0)
-						{//event
+						if (tier.getEvents().size() > 0) {// event
 							fileWriter.println();
-							for (Event event: tier.getEvents())
-							{
-								element= new StringBuilder();
+							for (Event event : tier.getEvents()) {
+								element = new StringBuilder();
 								element.append("<event");
-								if (event.getStart()!= null)
-									element.append(" start=\""+event.getStart().getId()+"\"");
-								if (event.getEnd()!= null)
-									element.append(" end=\""+event.getEnd().getId()+"\"");
-								if (event.isSetMedium())
-								{
-									if (event.getMedium()!= null)
-										element.append(" medium=\""+event.getMedium()+"\"");
+								if (event.getStart() != null)
+									element.append(" start=\"" + event.getStart().getId() + "\"");
+								if (event.getEnd() != null)
+									element.append(" end=\"" + event.getEnd().getId() + "\"");
+								if (event.isSetMedium()) {
+									if (event.getMedium() != null)
+										element.append(" medium=\"" + event.getMedium() + "\"");
 								}
-								if (event.getUrl()!= null)
-									element.append(" url=\""+event.getUrl()+"\"");
+								if (event.getUrl() != null)
+									element.append(" url=\"" + event.getUrl() + "\"");
 								element.append(">");
 								fileWriter.print(element);
-								{//ud-information
-									for (UDInformation udInfo: event.getUdInformations())
-									{
-										//udinfo cannot has an empty name
-										if (	(udInfo.getAttributeName()!= null) &&
-												(!udInfo.getAttributeName().equals("")))
-										{	
+								{// ud-information
+									for (UDInformation udInfo : event.getUdInformations()) {
+										// udinfo cannot has an empty name
+										if ((udInfo.getAttributeName() != null)
+												&& (!udInfo.getAttributeName().equals(""))) {
 											fileWriter.print("<ud-information");
-											fileWriter.print(" attribute-name=\""+udInfo.getAttributeName()+"\">");
-											if (udInfo.getValue() != null) 
+											fileWriter.print(" attribute-name=\"" + udInfo.getAttributeName() + "\">");
+											if (udInfo.getValue() != null)
 												fileWriter.print(udInfo.getValue());
 											fileWriter.print("</ud-information>");
 										}
 									}
 								}
-								//print text
-								if (	(event.getValue()!= null)&&
-										(!event.getValue().equals("")))
+								// print text
+								if ((event.getValue() != null) && (!event.getValue().equals("")))
 									fileWriter.print(event.getValue());
 								fileWriter.println("</event>");
 							}
 						}
 						fileWriter.println("</tier>");
-					}	
+					}
 					fileWriter.println("</basic-body>");
 				}
 				this.fileWriter.print("</basic-transcription>");
@@ -323,9 +299,7 @@ public class EXBWriter
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			throw e;
-		}
-		finally
-		{
+		} finally {
 			fileWriter.flush();
 			fileWriter.close();
 		}
